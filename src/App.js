9d53ce3d39baf9bd4241eba7970dafea13ea9d4d
Dhairya Upadhyay <94118693+Sharlock221b-svg@users.noreply.github.com>
2022-02-22 18:16:18 +0000

    Figured out the way to Add SOund

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 
 import './App.css';
+import React from "react"
+import Bsound from "./Components/Bsound"
 
 function App() {
+  const [qEle,setQelement] = React.useState(  {
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Heater-1',
+    url: `https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3`
+  },
+)
+  document.addEventListener('keydown', function(e) {
+      if(e.keyCode === qEle.keyCode){ 
+        document.getElementById('Q').play();
+      }
+  });
+
+
+  
   return (
-    <div className="App">
-   
+    <div className="App" id="drum-machine">
+      <audio id={qEle.keyTrigger} src={qEle.url} />
+       Q
     </div>
   );
 }
---
+++ src/Components/Bsound.js
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+
+export default function Bsound(props){
+  
+  return(
+    <div className="drum-pad">
+      
+    </div>
+  );
+}
